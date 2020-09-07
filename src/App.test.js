bc2b766e6593940f15352e1fd703967eb9e361e8
Marcelo Mena <marcelomena010@gmail.com>
2020-09-07 12:09:28 -0300

    Initialize template

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const history = createMemoryHistory();
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Router history={history}>
+      <App />
+    </Router>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "./assets/scss/style.scss";
+
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router history={history}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById("root")
 );
 
